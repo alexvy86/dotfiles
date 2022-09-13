7cee89833a2d2019a19971c4179ffd6e274b1036
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-09-13 01:25:48 -0500

    Install scoop to correct location

--- windows/.chezmoiscripts/run_once_before_01-install-scoop.ps1
+++ windows/.chezmoiscripts/run_once_before_01-install-scoop.ps1
@@ -1,13 +1,13 @@
+$StepName = "Installing scoop"
+Write-Host -ForegroundColor Cyan $StepName;
+
 # Install scoop if it's not installed yet
 if (-not (Get-Command "scoop" -ErrorAction SilentlyContinue)) {
+  $TmpPathToInstallScript = "$($env:TEMP)\install.ps1";
   # Use fixed version of the install script
-  Invoke-RestMethod https://raw.githubusercontent.com/ScoopInstaller/Install/d389cb126d1f4c9487ed4f84330961f700765fe2/install.ps1 | Invoke-Expression;
+  Invoke-RestMethod https://raw.githubusercontent.com/ScoopInstaller/Install/d389cb126d1f4c9487ed4f84330961f700765fe2/install.ps1 -OutFile $TmpPathToInstallScript;
+  Invoke-Expression "& $TmpPathToInstallScript -ScoopDir 'D:\Users\alexv\Tools\scoop'";
+  Remove-Item -Path $TmpPathToInstallScript;
 }
 
-# Install desired scoop buckets
-#scoop config aria2-enabled false
-#scoop install git
-#scoop bucket add extras
-#scoop bucket add twpayne https://github.com/twpayne/scoop-bucket
-#scoop bucket add nerd-fonts
-
+Write-Host -ForegroundColor Green "$StepName - Done";
