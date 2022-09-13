4da874e752cddb18cb274da01cf7445dfcee4a31
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-09-13 01:25:48 -0500

    Use pwsh instead of powershell

--- windows/.chezmoiscripts/run_once_before_00-powershell-setup.ps1
+++ windows/.chezmoiscripts/run_once_before_00-powershell-setup.ps1
@@ -1,9 +1,11 @@
+$PowershellExecutable = "pwsh.exe"; # Use "powershell.exe" for Windows Powershell (<= 5.0). Might need to update the interpreters.ps1.command in .chezmoi.yaml.tmpl
+
 # Self-elevate the script if required
 # Taken from chezmoi's documentation: https://www.chezmoi.io/user-guide/machines/windows/
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
   if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-    $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments;
+    Start-Process -FilePath $PowershellExecutable -Verb Runas -ArgumentList $CommandLine;
     Exit
   }
 }
