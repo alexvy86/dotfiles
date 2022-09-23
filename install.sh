82978f3eeab11c577001da49fa2d835826cb0026
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-09-23 14:49:11 -0500

    Try to get Codespaces install to work

--- install.sh
+++ install.sh
@@ -22,5 +22,10 @@ fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-# exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir" --debug
+
+# Init first to generate the chezmoi config file, which in my case overrides the sourceDir
+# based on the OS.
+"$chezmoi" init "--source=$script_dir" ;
+
+# Now run the actual chezmoi apply. exec will replace the current process with the one running chezmoi.
+exec "$chezmoi" apply --debug ;
