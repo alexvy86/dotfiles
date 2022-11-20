4f90a5b15c3c29809f16e7e177074a4f5cc7b396
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-11-19 22:26:26 -0600

    Improve script to install PowerShell

--- windows/.chezmoiscripts/run_once_before_00-install-powershell.bat
+++ windows/.chezmoiscripts/run_once_before_00-install-powershell.bat
@@ -1,4 +1,9 @@
-winget list --exact Microsoft.PowerShell
-if  %ERRORLEVEL% NEQ 0 (
+@ECHO OFF
+winget list --exact Microsoft.PowerShell >NUL  2>NUL
+if %ERRORLEVEL% NEQ 0 (
+    ECHO Installing PowerShell
     winget install Microsoft.PowerShell
+    ECHO PowerShell installed successfully
+) else (
+    ECHO PowerShell is already installed
 )
