a62850d79be8ed69adfc9580367d3537cb63bd8e
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2022-12-06 00:41:40 -0600

    Only change execution policy for current user

--- windows/.chezmoiscripts/run_once_before_01-powershell-setup.ps1
+++ windows/.chezmoiscripts/run_once_before_01-powershell-setup.ps1
@@ -1,14 +1,2 @@
-$PowershellExecutable = "pwsh.exe"; # Use "powershell.exe" for Windows Powershell (<= 5.0). Might need to update the interpreters.ps1.command in .chezmoi.yaml.tmpl
-
-# Self-elevate the script if required
-# Taken from chezmoi's documentation: https://www.chezmoi.io/user-guide/machines/windows/
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-    $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments;
-    Start-Process -FilePath $PowershellExecutable -Verb Runas -ArgumentList $CommandLine;
-    Exit
-  }
-}
-
-Set-ExecutionPolicy RemoteSigned -Scope LocalMachine;
+Write-Host "Setting execution policy for CurrentUser to RemoteSigned";
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;
