b21207cb94a88e91ef9e0cf2b8542ac67cb04b78
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-02-06 22:42:26 +0100

    Trust PSGallery PS module repo

--- windows/.chezmoiscripts/run_once_before_01-powershell-setup.ps1
+++ windows/.chezmoiscripts/run_once_before_01-powershell-setup.ps1
@@ -1,2 +1,5 @@
-Write-Host "Setting execution policy for CurrentUser to RemoteSigned";
+Write-Host "Set execution policy for CurrentUser to RemoteSigned";
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;
+
+Write-Host "Trust PSGallery repository";
+Set-PSRepository -InstallationPolicy Trusted -Name PSGallery;
