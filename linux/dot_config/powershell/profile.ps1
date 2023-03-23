7c3551a487161ffcf66a5b5af527d479a4f5e615
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-03-23 15:32:02 -0500

    Install pwsh and ohmyposh in linux; use my profile

---
+++ linux/dot_config/powershell/profile.ps1
@@ -0,0 +1,24 @@
+# This is the "Current user, all hosts" PowerShell profile file so it applies # to all host applications that host
+# PowerShell (e.g. PowerShell ISE, VSCode).
+
+# Import-Module Terminal-Icons;
+# Import-Module posh-git;
+# Import-Module git-aliases -DisableNameChecking;
+
+##
+# PSReadLine, see https://github.com/PowerShell/PSReadLine
+##
+
+## Make Tab behave like Ctrl+space for autocomplete
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete;
+
+# Make sure the cursor goes to the end of the line on history search
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd;
+
+Set-PSReadLineOption -PredictionSource History;
+
+# Set up fnm. Assumes it was installed earlier.
+fnm env --use-on-cd | Out-String | Invoke-Expression;
+
+# Initialize oh-my-posh
+oh-my-posh init pwsh --config "~/.config/alexvy86.omp.json" | Invoke-Expression;
