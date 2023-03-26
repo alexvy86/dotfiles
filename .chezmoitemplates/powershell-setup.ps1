e9d365712d31adae3ebe54c1f2566835b730b5af
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-03-26 03:18:20 -0500

    Powershell setup in Linux
    Set-ExecutionPolicy is not supported in Linux so it's now conditional on the OS.

--- .chezmoitemplates/powershell-setup.ps1
+++ .chezmoitemplates/powershell-setup.ps1
@@ -1,10 +1,15 @@
+{{- if eq .chezmoi.os "windows" }}
 Write-Host "Set execution policy for CurrentUser to RemoteSigned";
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;
+{{- end -}}
 
 Write-Host "Trust PSGallery repository";
 Set-PSRepository -InstallationPolicy Trusted -Name PSGallery;
 
 # Install modules
+Write-Host "Installing module git-aliases";
 Install-Module git-aliases -Scope CurrentUser -AllowClobber;
+Write-Host "Installing module posh-git";
 Install-Module posh-git -Scope CurrentUser -Force;
+Write-Host "Installing module Terminal-Icons";
 Install-Module Terminal-Icons; # Note: Terminal-Icons requires a font from https://www.nerdfonts.com/
