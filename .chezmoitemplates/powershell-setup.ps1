6a1961165009e765798b03a2e5dc3b447b34bc2a
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-03-26 03:24:13 -0500

    Fix spacing in template output

--- .chezmoitemplates/powershell-setup.ps1
+++ .chezmoitemplates/powershell-setup.ps1
@@ -1,7 +1,7 @@
 {{- if eq .chezmoi.os "windows" }}
 Write-Host "Set execution policy for CurrentUser to RemoteSigned";
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;
-{{- end -}}
+{{- end }}
 
 Write-Host "Trust PSGallery repository";
 Set-PSRepository -InstallationPolicy Trusted -Name PSGallery;
