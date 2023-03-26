f215b1b5119eba2023bd78842055380dbbad89e1
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-03-25 20:41:22 -0500

    Linux config updates
    - Move aliases outside of oh-my-zsh custom theme (prep to maybe use oh-my-posh instead)
    - Clean up comments from .zshrc
    - Replace deprecated oh-my-zsh update settings with newer alternatives

--- linux/dot_oh-my-zsh/custom/aliases-ff.zsh
+++
@@ -1,8 +0,0 @@
-# Sync my fork of the FF repo with upstream.
-# Needs a separate git fetch to be reflected locally.
-alias grsff="gh repo sync alexvy86/FluidFramework"
-
-# Common build commands
-alias ffb='npm run build'
-alias ffbc='npm run build:compile'
-alias ffbf='npm run build:fast'
