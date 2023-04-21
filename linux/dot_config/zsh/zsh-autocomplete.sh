387276a65c1c8d604bd49adf47156939a683b645
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-04-21 01:54:52 -0500

    Use zsh-autocomplete

---
+++ linux/dot_config/zsh/zsh-autocomplete.sh
@@ -0,0 +1,24 @@
+# Use fzf completion; then fall back to zsh's
+# ⚠️ NOTE: This setting can NOT be changed at runtime and requires that you have installed Fzf's shell extensions.
+zstyle ':autocomplete:*' fzf-completion yes
+
+# Start each new command line in live history search mode.
+zstyle ':autocomplete:*' default-context history-incremental-search-backward
+
+# Wait until this many characters have been typed, before showing completions.
+zstyle ':autocomplete:*' min-input 1  # int
+
+# If there are fewer than this many lines below the prompt, move the prompt up
+# to make room for showing this many lines of completions (approximately).
+# Applies to autocompletion, history search, and history menu.
+zstyle ':autocomplete:*' list-lines 8  # int
+
+# When completing, skip using the common prefix first as a result if there are several possible matches.
+zstyle ':autocomplete:*' insert-unambiguous no
+
+# zsh-autocomplete
+source ~/.config/zsh/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+
+# Make Tab go straight to the menu and cycle there
+bindkey '\t' menu-select "$terminfo[kcbt]" menu-select
+bindkey -M menuselect '\t' menu-complete "$terminfo[kcbt]" reverse-menu-complete
