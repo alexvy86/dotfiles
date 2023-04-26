52cab161cb09995165edc79b31d2d69f7b05eb80
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-04-26 05:21:41 -0500

    Split OhMyPosh setup to its own file

---
+++ linux/dot_config/use-ohmyposh.sh
@@ -0,0 +1,13 @@
+# Case insensitive, partial-word, and substring completion.
+# Doesn't handle case-insensitive match in the middle of a word.
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
+
+# Autoselect the first completion entry.
+setopt menu_complete
+
+# Prep so completion system can init correctly.
+# Copied from OhMyZsh's main script.
+autoload -U compaudit compinit zrecompile
+
+# Use Oh-My-Posh
+eval "$(oh-my-posh init zsh --config "~/.config/alexvy86.omp.json")"
