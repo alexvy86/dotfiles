51b7a017f6e301a39ee058c980c53d2d0023f527
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-05-06 00:58:03 -0500

    Be more strict about the install location of PowerShell on WIndows

--- windows/.chezmoiscripts/run_once_before_00-install-powershell.bat
+++ windows/.chezmoiscripts/run_once_before_00-install-powershell.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
-winget list --exact Microsoft.PowerShell >NUL  2>NUL
+winget list --exact Microsoft.PowerShell --source winget >NUL  2>NUL
 if %ERRORLEVEL% NEQ 0 (
     ECHO Installing PowerShell
-    winget install Microsoft.PowerShell
+    winget install --exact Microsoft.PowerShell --source winget
     ECHO PowerShell installed successfully
 ) else (
     ECHO PowerShell is already installed
