c785b89f1f1c943c19599dd7cb34a15b577485d2
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-05-06 16:32:53 -0500

    Auto-start ssh-agent in Windows. Commented alias to use it.

---
+++ windows/.chezmoiscripts/run_once_before_22-enable-ssh-agent.ps1
@@ -0,0 +1,19 @@
+$PowershellExecutable = "pwsh.exe"; # Use "powershell.exe" for Windows Powershell (<= 5.0). Might need to update the interpreters.ps1.command in .chezmoi.yaml.tmpl
+
+# Self-elevate the script if required
+# Taken from chezmoi's documentation: https://www.chezmoi.io/user-guide/machines/windows/
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments;
+    Start-Process -Wait -FilePath $PowershellExecutable -Verb Runas -ArgumentList $CommandLine;
+    Exit;
+  }
+}
+
+Write-Host "Setting 'ssh-agent' Windows service to automatic startup.";
+Set-Service -Name "ssh-agent" -StartupType Automatic;
+Start-Service -Name "ssh-agent";
+
+Write-Host -ForegroundColor Green "$StepName - Done";
+Write-Host "Press ENTER to continue";
+Read-Host;
