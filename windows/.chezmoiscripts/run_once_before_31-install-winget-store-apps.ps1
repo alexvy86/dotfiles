f57d1f198218795ba42570fe60bd8762648aa857
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-05-06 01:27:27 -0500

    Install MSStore apps with winget (not working)

---
+++ windows/.chezmoiscripts/run_once_before_31-install-winget-store-apps.ps1
@@ -0,0 +1,18 @@
+# TODO: figure out why this didn't work (in Windows 11)
+# DevToys and WhatsApp always complained of "Verifying/Requesting package acquisition failed: server error"
+
+# $Packages = @(
+# 	@{Id="9NBLGGH4NNS1"; Name="App Installer"},
+# 	@{Id="9PGCV4V3BK4W"; Name="Dev Toys"},
+# 	@{Id="9NKSQGP7F2NH"; Name="WhatsApp"}
+# 	);
+
+# $Packages | ForEach-Object {
+# 	winget list --exact --id $_.Id --source msstore > $null;
+# 	if ($LastExitCode -eq 0) {
+# 		Write-Host "Package $($_.Name) ($($_.Id)) is already installed";
+# 	} else {
+# 		Write-Host "Installing MSStore app $($_.Name) ($($_.Id))";
+# 		winget install --exact --id $_.Id --source msstore --accept-package-agreements --accept-source-agreements;
+# 	}
+# }
