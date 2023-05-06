c40308c06b5069db2c5b4b018a1818a6bb9c6a8e
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-05-06 02:21:12 -0500

    More registry customizations for Windows

--- windows/.chezmoiscripts/run_once_before_21-windows-customization.ps1
+++ windows/.chezmoiscripts/run_once_before_21-windows-customization.ps1
@@ -1,11 +1,54 @@
 $settingsToChange = @();
 
-# Disable browser tabs showing up in Alt-Tab. Works in Windows 10 and 11.
+# Disable browser tabs showing up in Alt-Tab. Applies to Windows 10 and 11.
 $settingsToChange += @{
 	Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced";
 	Name = "MultiTaskingAltTabFilter";
 	DesiredValue = 3;
 };
+
+# Hide the "Widgets" button in the taskbar. Applies to Windows 11.
+$settingsToChange += @{
+	Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced";
+	Name = "TaskbarDa";
+	DesiredValue = 0;
+};
+
+# Hide the "TaskView" button in the taskbar. Applies to Windows 10 and 11.
+$settingsToChange += @{
+	Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced";
+	Name = "ShowTaskViewButton";
+	DesiredValue = 0;
+};
+
+# Hide the "Chat" button in the taskbar. Applies to Windows 11.
+$settingsToChange += @{
+	Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced";
+	Name = "TaskbarMn";
+	DesiredValue = 0;
+};
+
+# Left-align the Start Menu. Applies to Windows 11.
+$settingsToChange += @{
+	Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced";
+	Name = "TaskbarAl";
+	DesiredValue = 0;
+};
+
+# Show hidden files in Windows Explorer. Applies to Windows 10 and 11.
+$settingsToChange += @{
+	Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced";
+	Name = "Hidden";
+	DesiredValue = 1;
+};
+
+# Show file extensions in Windows Explorer. Applies to Windows 10 and 11.
+$settingsToChange += @{
+	Path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced";
+	Name = "HideFileExt";
+	DesiredValue = 0;
+};
+
 
 # Apply registry changes
 Write-Host "Updating registry settings";
