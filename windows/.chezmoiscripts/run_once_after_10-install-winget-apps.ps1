91355430640086c5c1cc1a7b9dd318555dfc5928
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2023-05-20 17:29:09 -0500

    Automate git for windows installation

--- windows/.chezmoiscripts/run_once_after_10-install-winget-apps.ps1
+++ windows/.chezmoiscripts/run_once_after_10-install-winget-apps.ps1
@@ -1,38 +1,46 @@
 $StepName = "Installing regular applications with winget";
 Write-Host -ForegroundColor Cyan $StepName;
 
-$Packages = @(
-	"7zip.7zip",                      # Has UAC prompt
-	"Microsoft.PowerToys",
-	"Git.Git",                        # Has UAC prompt
-	"GitHub.GitLFS",                  # Has UAC prompt
-	"JanDeDobbeleer.OhMyPosh",
-	"Microsoft.VisualStudioCode",
-	"Microsoft.Edge.Dev"              # Has UAC prompt
-	"Google.Chrome",
-	"AgileBits.1Password",            # Has UAC prompt
-	"ScooterSoftware.BeyondCompare4"  # Has UAC prompt
-	# "Docker.DockerDesktop",
-	# "Microsoft.AzureCLI",
-	# "PuTTY.PuTTY",
-	# "Notepad++.Notepad++",
-	# "VideoLAN.VLC",
-	# "Discord.Discord",
-	# "PrivateInternetAccess.PrivateInternetAccess",
-	# "Windscribe.Windscribe",
-	# "Spotify.Spotify",
-	# "Valve.Steam",
-	# "tailscale.tailscale",
-	);
+$PathToGitInstallerSettings = Join-Path -Path $env:USERPROFILE -ChildPath ".config/git-for-windows-installer-settings.ini";
 
-$Packages | ForEach-Object {
-	winget list --exact --id $_ --source winget > $null;
+$ApplicationsToInstall = @(
+	@{Id = "7zip.7zip"; },
+	@{Id = "Microsoft.PowerToys"; }, # Args = "/passive" - Will this work? Not sure if winget uses the correct installer executable that can take this arg
+	@{Id = "Git.Git"; Args = "/SILENT /LOADINF=$PathToGitInstallerSettings" },
+	@{Id = "GitHub.GitLFS"; },
+	@{Id = "JanDeDobbeleer.OhMyPosh"; },
+	@{Id = "Microsoft.VisualStudioCode"; }, # Args = "/SILENT"
+	@{Id = "Microsoft.Edge.Dev"; },
+	@{Id = "Google.Chrome"; },
+	@{Id = "AgileBits.1Password"; },
+	@{Id = "ScooterSoftware.BeyondCompare4"; }
+	# @{Id="Docker.DockerDesktop",
+	# @{Id="Microsoft.AzureCLI",
+	# @{Id="PuTTY.PuTTY",
+	# @{Id="Notepad++.Notepad++",
+	# @{Id="VideoLAN.VLC",
+	# @{Id="Discord.Discord",
+	# @{Id="PrivateInternetAccess.PrivateInternetAccess",
+	# @{Id="Windscribe.Windscribe",
+	# @{Id="Spotify.Spotify",
+	# @{Id="Valve.Steam",
+	# @{Id="tailscale.tailscale",
+);
+
+foreach ($app in $ApplicationsToInstall) {
+	winget list --exact --id $app.Id --source winget > $null;
 	if ($LastExitCode -eq 0) {
-		Write-Host "Package $_ is already installed";
-	} else {
-		Write-Host "Installing package $_";
+		Write-Host "Package $($app.Id) is already installed";
+	}
+ else {
+		Write-Host "Installing package $($app.Id)";
 		# TODO: Use --override to do automated installations of everything, e.g. with /SILENT for VSCode or -passive for PowerToys
-		winget install --exact --id $_ --source winget;
+		if ($null -eq $app.Args) {
+			winget install --exact --id $app.Id --source winget;
+		}
+		else {
+			winget install --exact --id $app.Id --source winget --override "$($app.Args)";
+		}
 	}
 }
 
