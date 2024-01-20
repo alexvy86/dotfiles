064545f8e4c21360f4d09e6b9a41f8255c4ef37c
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2024-01-19 21:52:10 -0600

    Handle error when winget cannot be found

--- windows/.chezmoiscripts/run_once_before_00-install-powershell.bat
+++ windows/.chezmoiscripts/run_once_before_00-install-powershell.bat
@@ -1,8 +1,11 @@
 @ECHO OFF
-winget list --exact Microsoft.PowerShell --source winget >NUL  2>NUL
-if %ERRORLEVEL% NEQ 0 (
+WHERE winget >NUL 2>NUL
+IF %ERRORLEVEL% NEQ 0 ECHO 'winget' was not found && EXIT /b 1
+
+winget list --exact Microsoft.PowerShell --source winget >NUL 2>NUL
+IF %ERRORLEVEL% NEQ 0 (
     ECHO Installing PowerShell
-    winget install --exact Microsoft.PowerShell --source winget
+    winget install --exact Microsoft.PowerShell --source winget || EXIT /b 1
     ECHO PowerShell installed successfully
 ) else (
     ECHO PowerShell is already installed
