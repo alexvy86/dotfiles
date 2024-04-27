49cd08e01ea42ef7712fac56ef03e7a5e256823d
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2024-04-27 17:23:52 -0500

    Fail step to install/configure PowerToys if winget configure fails

--- windows/.chezmoiscripts/run_once_after_11-install-powertoys.ps1
+++ windows/.chezmoiscripts/run_once_after_11-install-powertoys.ps1
@@ -1,6 +1,9 @@
+$ErrorActionPreference = "Stop";
 $StepName = "Installing and configuring PowerToys";
 Write-Host -ForegroundColor Cyan $StepName;
 
 winget configure --accept-configuration-agreements --file "$env:USERPROFILE/.config/powertoys.dsc.yaml";
+
+if ($LASTEXITCODE -ne 0) { throw "Failed to apply Winget configuration. Exit code: $LASTEXITCODE" }
 
 Write-Host -ForegroundColor Green "$StepName - Done";
