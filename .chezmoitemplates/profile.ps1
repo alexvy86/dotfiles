47f0aeecb9c99e3d2782ca2006702b11116248d0
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2025-01-18 01:00:38 -0600

    Add autocomplete for winget commands in Windows

--- .chezmoitemplates/profile.ps1
+++ .chezmoitemplates/profile.ps1
@@ -1,5 +1,17 @@
 Set-StrictMode -Version Latest;
 
+# Auto-completions for Winget
+# https://learn.microsoft.com/en-us/windows/package-manager/winget/tab-completion
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+	param($wordToComplete, $commandAst, $cursorPosition)
+			[Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+			$Local:word = $wordToComplete.Replace('"', '""')
+			$Local:ast = $commandAst.ToString().Replace('"', '""')
+			winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+					[System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+			}
+}
+
 # TODO: There might be something interesting we can do with $PSDefaultParameterValues
 
 #----------------------------------------------------------
