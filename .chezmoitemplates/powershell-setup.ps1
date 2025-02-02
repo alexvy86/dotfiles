d9ae51a70a770e6f53cc6d754a7f300d191ffbdc
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2025-02-02 16:01:05 -0600

    Install PS modules outside Documents folder in Windows

--- .chezmoitemplates/powershell-setup.ps1
+++ .chezmoitemplates/powershell-setup.ps1
@@ -1,17 +1,28 @@
 $StepName = "General PowerShell setup";
 Write-Host -ForegroundColor Cyan $StepName;
 
+Write-Host "Trust PSGallery repository";
+Set-PSRepository -InstallationPolicy Trusted -Name PSGallery;
+
 {{ if eq .chezmoi.os "windows" -}}
 
 Write-Host "Set execution policy for CurrentUser to RemoteSigned";
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;
 
-{{ end -}}
+# Install modules.
+# On Windows, use scoop so they are installed to a folder outside My Documents which is in OneDrive.
+Write-Host "Installing module git-aliases";
+scoop install git-aliases;
+Write-Host "Installing module posh-git";
+scoop install posh-git;
+Write-Host "Installing module Terminal-Icons";
+scoop install terminal-icons; # Note: Terminal-Icons requires a font from https://www.nerdfonts.com/
+Write-Host "Installing module DockerCompletion";
+scoop install DockerCompletion;
 
-Write-Host "Trust PSGallery repository";
-Set-PSRepository -InstallationPolicy Trusted -Name PSGallery;
+{{ else }}
 
-# Install modules
+# Install modules.
 Write-Host "Installing module git-aliases";
 Install-Module git-aliases -Scope CurrentUser -AllowClobber;
 Write-Host "Installing module posh-git";
@@ -20,5 +31,7 @@ Write-Host "Installing module Terminal-Icons";
 Install-Module Terminal-Icons; # Note: Terminal-Icons requires a font from https://www.nerdfonts.com/
 Write-Host "Installing module DockerCompletion";
 Install-Module DockerCompletion;
+
+{{ end -}}
 
 Write-Host -ForegroundColor Green "$StepName - Done";
