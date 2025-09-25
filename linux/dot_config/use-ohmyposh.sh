ba00a99faae6baafaf27a2ad5ee96f78b5195651
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2025-09-24 22:16:11 -0500

    Add shellcheck

--- linux/dot_config/use-ohmyposh.sh
+++ linux/dot_config/use-ohmyposh.sh
@@ -1,6 +1,9 @@
+# shellcheck shell=bash
+
 # Case insensitive, partial-word, and substring completion.
 # Doesn't handle case-insensitive match in the middle of a word.
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
 
 # Use Oh-My-Posh
-eval "$(oh-my-posh init zsh --config "~/.config/alexvy86.omp.json")"
+# shellcheck disable=SC2312
+eval "$(oh-my-posh init zsh --config "${HOME}/.config/alexvy86.omp.json")"
