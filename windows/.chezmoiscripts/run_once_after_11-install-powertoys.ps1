da5d29f1f208960ffa5fcf37bf38f81890ac58ac
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2025-11-04 17:21:14 -0600

    Fix install + configuration of PowerToys

--- windows/.chezmoiscripts/run_once_after_11-install-powertoys.ps1
+++ windows/.chezmoiscripts/run_once_after_11-install-powertoys.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = "Stop";
 $StepName = "Installing and configuring PowerToys";
 Write-Host -ForegroundColor Cyan $StepName;
 
+winget configure --enable;
 winget configure --accept-configuration-agreements --file "$env:USERPROFILE/.config/powertoys.dsc.yaml";
 
 if ($LASTEXITCODE -ne 0) { throw "Failed to apply Winget configuration. Exit code: $LASTEXITCODE" }
