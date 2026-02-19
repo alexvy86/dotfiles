9c4f0dda12220e099a6227c6b9b611c46591e8fc
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2026-02-18 23:59:24 -0600

    Refactor scoop setup and improve error handling

--- home/.chezmoiscripts/windows/run_once_after_00-install-scoop.ps1
+++ home/.chezmoiscripts/windows/run_once_after_00-install-scoop.ps1
@@ -1,5 +1,11 @@
-$StepName = "Installing scoop"
+$StepName = "Installing scoop and adding buckets";
 Write-Host -ForegroundColor Cyan $StepName;
+
+function Update-Path {
+  $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") +
+              ";" +
+              [System.Environment]::GetEnvironmentVariable("Path","User")
+}
 
 # Install scoop if it's not installed yet
 if (-not (Get-Command "scoop" -ErrorAction SilentlyContinue)) {
@@ -9,5 +15,17 @@ if (-not (Get-Command "scoop" -ErrorAction SilentlyContinue)) {
   Invoke-Expression "& $TmpPathToInstallScript -ScoopDir '$env:WIN_TOOLS_PATH/scoop'";
   Remove-Item -Path $TmpPathToInstallScript;
 }
+
+Update-Path;
+
+# Unfortunately, scoop installs itself as a ps1 script that won't cause an error to
+# terminate the calling script even when $PSNativeCommandUseErrorActionPreference = $true
+# plus $ErrorActionPreference = "Stop". So we need to check the exit code manually.
+scoop bucket add extras;
+if ($LASTEXITCODE -ne 0) { throw "Failed to install bucket 'extras'. Exit code: $LASTEXITCODE" }
+scoop bucket add nerd-fonts;
+if ($LASTEXITCODE -ne 0) { throw "Failed to install bucket 'nerd-fonts'. Exit code: $LASTEXITCODE" }
+scoop update;
+if ($LASTEXITCODE -ne 0) { throw "Failed to update scoop. Exit code: $LASTEXITCODE" }
 
 Write-Host -ForegroundColor Green "$StepName - Done";
